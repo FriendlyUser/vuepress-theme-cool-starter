26dc9bf4c9d1bd6e19b815e2a3e186469483a21f
dli <davidli012345@gmail.com>
2020-04-18 15:40:58 -0700

    switching to commas

--- addAds.sh
+++ addAds.sh
@@ -3,5 +3,5 @@
 if [ "$TRAVIS_REPO_SLUG" = "FriendlyUser/vuepress-theme-cool-starter" ]
 then
   echo google.com, pub-2479144310234386, DIRECT, f08c47fec0942fa0 > ads.txt
-  sed -i '14i [ "script": { src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", "data-ad-client": "ca-pub-2479144310234386", async: true } ]' docs/.vuepress/config.js
+  sed -i '14i [ "script", { src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", "data-ad-client": "ca-pub-2479144310234386", async: true } ]' docs/.vuepress/config.js
 fi
