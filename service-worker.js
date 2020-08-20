1269a7460f3757256f115a364a643ffa65704e06
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-20 15:35:35 +0000

    Deploy FriendlyUser/vuepress-theme-cool-starter to github.com/FriendlyUser/vuepress-theme-cool-starter.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -27,66 +27,66 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "f8d80c82529b5e6ed4ead71c7c4be251"
+    "revision": "2a24983d0109d8967e1510f4e8c12f13"
   },
   {
     "url": "admonitions.html",
-    "revision": "95f4a98f251e5a0bfdc31af3b60eb637"
+    "revision": "a035726a9b9870e60175315308f3242b"
   },
   {
-    "url": "assets/css/0.styles.4af44b62.css",
-    "revision": "40803a96c6eede13cb57a96dfb4322db"
+    "url": "assets/css/0.styles.20ecc5df.css",
+    "revision": "684007626a67715e1e7befd1b3bc7fce"
   },
   {
     "url": "assets/img/search.83621669.svg",
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.0b4217d2.js",
+    "url": "assets/js/10.a06a757e.js",
     "revision": "9e6720f8ea285a4dddd58199763163d1"
   },
   {
-    "url": "assets/js/11.b4b960bb.js",
+    "url": "assets/js/11.90494806.js",
     "revision": "ca4bab5d2a42a6aae13177521007a475"
   },
   {
-    "url": "assets/js/12.50b8f0b6.js",
+    "url": "assets/js/12.5929a623.js",
     "revision": "84c1db64cc678f8c2643d6017074e921"
   },
   {
-    "url": "assets/js/13.9e982ce6.js",
+    "url": "assets/js/13.ccef8aff.js",
     "revision": "e1d02cd9c31ba9f1ca71e68199695f8a"
   },
   {
-    "url": "assets/js/14.9c13ceea.js",
+    "url": "assets/js/14.337f470d.js",
     "revision": "134f39c295640e281b3b2e61fc80c3da"
   },
   {
-    "url": "assets/js/15.b516cc3d.js",
+    "url": "assets/js/15.718505bc.js",
     "revision": "0c7fe564ea5d444ebfcdd5142f82db4e"
   },
   {
-    "url": "assets/js/16.2a890efe.js",
-    "revision": "2b11119703ee3036ffef420d46562950"
+    "url": "assets/js/16.2d271377.js",
+    "revision": "b1b4880d701b12dbf7457dc3fc81321d"
   },
   {
-    "url": "assets/js/17.e445487d.js",
-    "revision": "85d3675d75a6d91119601d25158f0d1b"
+    "url": "assets/js/17.2f448437.js",
+    "revision": "3fdbd6bb6f55450019e63395409d487e"
   },
   {
     "url": "assets/js/18.83cd041d.js",
     "revision": "d29991e40ccbcfab884130f09e0c9d91"
   },
   {
-    "url": "assets/js/2.7ecc87dd.js",
-    "revision": "e5723c90dda2f66e7e963a824e578474"
+    "url": "assets/js/2.96e286a2.js",
+    "revision": "1de73debce8ddd19f30b147ca5d43114"
   },
   {
-    "url": "assets/js/3.4704c491.js",
+    "url": "assets/js/3.225b9a5e.js",
     "revision": "02d6ced24bb69f91f32d9be49784ea8b"
   },
   {
-    "url": "assets/js/4.71ab9b1c.js",
+    "url": "assets/js/4.a4a9a7b3.js",
     "revision": "abc0f82cd550517f0c6adcc340d3b84a"
   },
   {
@@ -94,7 +94,7 @@ self.__precacheManifest = [
     "revision": "40765b2f9ec1f6f6307649b663b65cc8"
   },
   {
-    "url": "assets/js/6.f73a67b1.js",
+    "url": "assets/js/6.36b2b953.js",
     "revision": "0da6df7934796aed13939ed8c8950443"
   },
   {
@@ -102,7 +102,7 @@ self.__precacheManifest = [
     "revision": "70666f2801755c256b5407016c50baed"
   },
   {
-    "url": "assets/js/8.604bed6e.js",
+    "url": "assets/js/8.470abded.js",
     "revision": "951541e5e674d7f21ee7c530f309a13e"
   },
   {
@@ -110,24 +110,24 @@ self.__precacheManifest = [
     "revision": "68e8171b4f7cf27d3e40f5eef0e8c7a0"
   },
   {
-    "url": "assets/js/app.1383176d.js",
-    "revision": "2d2c5c822931db92f0e6e6feae3a002d"
+    "url": "assets/js/app.134f9c5f.js",
+    "revision": "295927326673cdb9e1f520d13023eaf8"
   },
   {
     "url": "charts-math.html",
-    "revision": "72473cb8f69caebf0cfe0a0f3dd4b4af"
+    "revision": "18a8f7012f30c12e08988d9a116fabd9"
   },
   {
     "url": "custom-components.html",
-    "revision": "1bad35619221b2b0db1e9df5da1f33d4"
+    "revision": "d29d7ef746e624de2dbd9c0b5b85d83b"
   },
   {
     "url": "diagrams.html",
-    "revision": "dc16e22650287962b92afe2b25580ab1"
+    "revision": "68fba836a7f34c4f6bfada1004839789"
   },
   {
     "url": "index.html",
-    "revision": "295730f91d25fe1d2f5da3fadd2e7dec"
+    "revision": "e408e15d36cc712f283c81697b732bf5"
   },
   {
     "url": "myAvatar.png",
